27b2206bd427ddfed147e440bf943213c640b600
vasanth <70842484+vasanthfeb@users.noreply.github.com>
2025-01-21 14:46:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// pre loder start
+window.addEventListener("load", () => {
+  // Get the time to ensure the preloader stays visible for a minimum time
+  const minDuration = 1000; // Minimum duration for preloader in milliseconds
+  const startTime = Date.now();
+
+  // Apply overflow hidden to the body to prevent scrolling
+  document.body.style.overflow = 'hidden';
+
+  setTimeout(() => {
+    const elapsedTime = Date.now() - startTime;
+    const delay = Math.max(minDuration - elapsedTime, 0); // Ensure at least 1 second
+
+    setTimeout(() => {
+      // Hide preloader
+      document.getElementById('preloader').style.display = 'none';
+      
+      // Show the main content
+      // document.getElementById('content').style.display = 'block';
+      
+      // Restore normal page scroll behavior
+      document.body.style.overflow = 'auto'; // Enable scrolling again
+    }, delay);
+  }, 0);
+});
+
 
 // Team Section start
 
